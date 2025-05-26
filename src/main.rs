60d4510cd871e0c7b07e316bb8d69f111b50df6f
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-05-26 23:50:59 +0200

    Finished login system + signature generation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::fmt::format;
 use std::{fs, io::BufReader};
 use std::{env, io};
 use std::fs::File;
 
 use dotenv::dotenv;
 use futures_util::{SinkExt, StreamExt};
-use plume_core::config;
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 mod commands;
@@ -25,17 +23,24 @@ async fn main() {
 
     let mut configs: plume_core::config::Config = serde_json::from_reader(reader).expect("Unable to convert this file to json");
 
+    let mut signing_key = String::new();
+    let mut public_key = String::new();
 
     if configs.me.public_ed_path.len() == 0 {
         // Then generate public keys
         let (private_ed, public_ed) = plume_core::encryption::generate_ed_keys();
         println!("Private : {}", private_ed);
         println!("Public : {}", public_ed);
+        signing_key = private_ed;
+        public_key = public_ed;
     }
 
     match fs::read(&configs.me.public_ed_path) {
         Ok(file) => {
-            println!("Your public key is : \n{}", String::from_utf8(file).expect("Unable to transform file to string"));
+            let public_ed = String::from_utf8(file).expect("Unable to transform file to string");
+            println!("Your public key is : \n{}", public_ed);
+            signing_key = String::from_utf8(fs::read(&configs.me.private_ed_path).expect("Invalid file storing signing key")).expect("Invalid key stored in file");
+            public_key = public_ed;
         },
         Err(err) => {
             println!("Key path : {}", configs.me.public_ed_path);
@@ -44,8 +49,9 @@ async fn main() {
 
             let (private_ed, public_ed) = plume_core::encryption::generate_ed_keys();
             // write the keys to files
-            fs::write(format!("{}/keys/private_ed.pem", config_path), private_ed).expect("Unable to write key to file");
+            fs::write(format!("{}/keys/private_ed.pem", config_path), &private_ed).expect("Unable to write key to file");
             fs::write(format!("{}/keys/public_ed.pem", config_path), &public_ed).expect("Unable to write key to file");
+
 
             println!("Wrote keys to files");
 
@@ -60,6 +66,9 @@ async fn main() {
             println!("Config file updated");
 
             println!("\n\n Your public key is : \n{}", public_ed);
+
+            signing_key = private_ed;
+            public_key = public_ed;
         }
     }
 
@@ -68,6 +77,13 @@ async fn main() {
     println!("Connected to server !");
 
     let (mut write, mut read) = ws_stream.split();
+
+    // ------ Login ------
+    let signed_message = plume_core::encryption::sign_packet(format!("login__{}", public_key), &signing_key);
+    let message = Message::text(signed_message);
+
+    write.send(message).await.expect("Failde to send message to server");
+    // ------ Login ------
 
     // Spawning the sender in a background task, not
     tokio::spawn(async move{
@@ -87,8 +103,6 @@ async fn main() {
 
         input = input[..input.len()-1].to_string();
 
-        println!("Input is : '{}'", input);
-        
         // Command recognition
         if let Some(command) = commands::command_list().into_iter().find(|cmd| cmd == &input) {
             commands::execute_command(&command);
@@ -96,7 +110,10 @@ async fn main() {
         };
 
         // If no command recognized then sending the message
-        let message = Message::text(input);
+        // Before seding: signing 
+
+        let signed_message = plume_core::encryption::sign_packet(format!("message__{}__{}__{}", public_key, "future_target" , input), &signing_key);
+        let message = Message::text(signed_message);
 
         write.send(message).await.expect("Failde to send message to server");
     }
