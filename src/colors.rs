af5e73e5ba09d0d06ee0f02863280e8204934794
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-05-29 14:22:10 +0200

    Added error & warning colors

--- src/colors.rs
+++ src/colors.rs
@@ -5,3 +5,11 @@ pub fn info(text: &str) -> String {
 pub fn message(text: &str) -> String {
     format!("\x1b[34m{}\x1b[0m", text)
 }
+
+pub fn error(text: &str) -> String {
+    format!("\x1b[31m{}\x1b[0m", text)
+}
+
+pub fn warning(text: &str) -> String {
+    format!("\x1b[33m{}\x1b[0m", text)
+}
